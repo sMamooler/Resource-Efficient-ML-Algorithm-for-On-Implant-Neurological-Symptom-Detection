e980426bd579437743bb5b11db5ce9415b28908c
Sepideh Mamooler <39418356+sMamooler@users.noreply.github.com>
2020-12-17 10:11:22 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -26,6 +26,8 @@ class LSTM(nn.Module):
             number of hidden nodes of eacch lstm layer
         n_layers: int
             number of layers in each lstm layer
+        fixed_pt_quantize: boolean
+            if set fixed point qunatization will a applied
       
         """
         super().__init__()
@@ -34,6 +36,8 @@ class LSTM(nn.Module):
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.seq_len = seq_len
+
+        #used in fixed point quantization
         self.fixed_pt_quantize = fixed_pt_quantize
 
 
@@ -44,13 +48,13 @@ class LSTM(nn.Module):
         
         #lstm layers
         self.lstm = nn.LSTM(self.input_dim, self.n_hidden, self.n_layers, batch_first=False)
-        #self.lstm2 = nn.LSTM(self.n_hidden, self.n_hidden, self.n_layers, batch_first=False)
         #linear layer
         self.fc = nn.Linear(self.n_hidden, self.output_dim)
+        #activation function
         self.act = nn.Sigmoid()
     
     
-    def binarize_weights(self):
+    def binarize_weights(self, ind_layer):
 
         """
         Function that binarizes the parameters of the given layer
@@ -61,8 +65,9 @@ class LSTM(nn.Module):
             the index of the layer to be binarized
       
         """ 
-        
-        net = self.lstm
+        net = self.lstm2
+        if ind_layer == 0:
+            net = self.lstm
         for item in net.state_dict().items():
             weight_name = item[0]
             weights_mat = item[1]
@@ -77,7 +82,6 @@ class LSTM(nn.Module):
             
             
     def quantize_fixed_pt(self):
-
         """
         Function that does 3 decimal fixed point quantization on the weights of the layer
         Parameters
@@ -103,12 +107,12 @@ class LSTM(nn.Module):
         """
 
         for k in range(self.n_layers):
-            parameters_to_prune = ((self.lstm, "weight_ih_l"+str(k)), (self.lstm, "weight_hh_l"+str(k)) )
+            parameters_to_prune = ((self.lstm, "weight_ih_l"+str(k)), (self.lstm, "weight_hh_l"+str(k)))
             prune.global_unstructured(parameters_to_prune, pruning_method=ThresholdPruning, threshold= 0.125)
         
 
 
-    def forward(self, input, hidden, quant=False):
+    def forward(self, input, hidden, bin_=False, quant=False):
         """
         Function that forward pass through the network 
 
@@ -118,6 +122,8 @@ class LSTM(nn.Module):
             contain feature vectors of train data
         hidden: pair of matrices of shape [n_layer, batch_size, n_hidden]
             hidden/cell state of LSTM layers
+        bin_: boolean
+            if set, the network is binarized
         quant: boolean
             if set, the network is quantized
         Returns
@@ -135,10 +141,13 @@ class LSTM(nn.Module):
 
            
             r_output, hidden = self.lstm(new_input, hidden)
+           
         
             ## put x through the fully-connected layer
             out = self.act(r_output)
-            out = self.fc(out)           
+            out = self.fc(out)
+           
+           
             
             return out, hidden
 
@@ -151,7 +160,7 @@ class LSTM(nn.Module):
         
             self.quantize_fixed_pt()
             r_output, hidden = self.lstm(new_input, hidden)
-        
+            
             ## put x through the fully-connected layer
             out = self.act(r_output)
             out = self.fc(out)
@@ -198,7 +207,6 @@ class LSTM(nn.Module):
             
 
             r_output, hidden = self.lstm(new_input, hidden)
-            #r_output, hidden = self.lstm2(r_output, hidden)
             out = self.act(r_output)
             out = self.fc(out)
             
@@ -209,72 +217,7 @@ class LSTM(nn.Module):
         
             return out, hidden
     
-    def quantized_predict(self, input, hidden):
-        """
-        Function that forward pass through the quantized network
-
-        Parameters
-        ----------
-        TrainX: matrix of shape [#datapoints, seq_len, input_dim]
-            contain feature vectors of train data
-        hidden: pair of matrices of shape [n_layer, batch_size, n_hidden]
-            hidden/cell state of LSTM layers
-
-        Returns
-        -------
-        out: matrix of shape [seq_len, batch_size, n_hidden]
-            scaler used to standardize the data
-        hidden: pair of matrices of shape [n_layer, batch_size, n_hidden]
-            hidden/cell state of LSTM layers
-        """ 
-
-       
-        orig_weights = {}
-        old_state_dict = copy.deepcopy(self.state_dict())
-        tmp_state_dict = copy.deepcopy(self.state_dict())
-        
-        #computes the final parameters from quantized parameters and centroids 
-        for item in self.state_dict().items():
-                
-            weight_name = item[0]
-            weights_mat = item[1]
-            orig_weights[weight_name] = weights_mat.clone()
-           
-
-            centroids = self.centroids[weight_name]
-            weights_vect = torch.flatten(weights_mat)
-           
-            indices = torch.LongTensor(weights_vect.long())
-            for i in range(weights_vect.shape[0]):
-                weights_vect[i] = centroids[indices[i]]
-                
-
-         
-            mat = weights_vect.reshape(weights_mat.shape)
-
-            
-            tmp_state_dict[weight_name].copy_(mat)
-            
-        #update the model's state_dict with final parameters 
-        self.load_state_dict(tmp_state_dict) 
-
-       
-        ##our input has shape [batch_size, seq_len, input_dim] but lstm wants [seq_len, batch_size, input_dim], we need to reconstrcut the input the way lstm wants:
-        new_input = torch.ones((self.seq_len, input.shape[0], self.input_dim))
-        for i in range(self.seq_len):
-            new_input[i] = input[:,i,:]
-        input = new_input
-
-        r_output, hidden = self.lstm(new_input, hidden)
-        r_output, hidden = self.lstm2(r_output, hidden)
-        out = self.fc(r_output)
-        
-        #change the weights back to quantized parameters once done with prediction
-        self.load_state_dict(old_state_dict)
-
     
-        return out, hidden
-
 
     def init_hidden(self, batch_size):
         """
--- run.py
+++ run.py
@@ -23,16 +23,22 @@ figure_path = os.path.join(root, 'figures')
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--pre_trained", help="Set to True if you want to use pre-trained model", type=bool)
+parser.add_argument("--pre_trained", default=False, help="Set to True if you want to use pre-trained model", type=bool)
+parser.add_argument("--fixed_pt_quantization", default=False, help="Set to True if you want to use fixed point quantization", type=bool)
+parser.add_argument("--pruning", default=False, help="Set to True if you want to use pruning", type=bool)
+parser.add_argument("--trained_quantization", default=False, help="Set to True if you want to use trained quantization", type=bool)
+
 args = parser.parse_args()
 pre_trained = args.pre_trained
-
+fixed_pt_quantize = args.fixed_pt_quantization
+pruning = args.pruning
+trained_quantization = args.trained_quantization
 
 
 for Idx_subject in list([10]): # 3 subjects index 10-12
 
        
-        for Finger in list([0]): # 5 fingers for each subject. 0:thumb, 1:index, 2:middle ...
+        for Finger in list([0,1,2,3,4]): # 5 fingers for each subject. 0:thumb, 1:index, 2:middle ...
             
             #load training data (TrainX: feature vectors, TrainY: labels)
             matData = sio.loadmat(data_path + '/BCImoreData_Subj_'+str(Idx_subject)+'_200msLMP.mat')
@@ -67,41 +73,128 @@ for Idx_subject in list([10]): # 3 subjects index 10-12
             # Now, we reconstructed TrainX/TestX to have a shape (num_of_samples, sequence_length, input_size)
             # We can fit this to the LSTM
             
-            print("Run for finger "+str(Finger))
+            print("Run for subject " + str(Idx_subject) + " finger "+str(Finger))
 
             n_hidden = 20
             n_layers = 5
-            n_epochs =  60 
             input_dim = TrainX.shape[2]
             output_dim = TrainY.shape[1]
             seq_len =  TrainX.shape[1]
 
-            net = LSTM(input_dim, output_dim, seq_len,  n_hidden, n_layers)
+            net = LSTM(input_dim, output_dim, seq_len,  n_hidden, n_layers, fixed_pt_quantize = fixed_pt_quantize)
 
             lossfunc = nn.MSELoss()
 
             optimizer = torch.optim.Adamax(net.parameters(), lr=0.002)
-            net.train()
 
-
+           
+            print("Baseline ===================================================================")
             ##training the initial model
+            if fixed_pt_quantize:
+                figure_name = "/Subject_" + str(Idx_subject) + "_Finger_"+str(Finger)+"_fp_quant"
+                PATH_pre_trained = checkpoint_path + '/s'+ str(Idx_subject) + '_f'+str(Finger)+'_fp_trained_model'
+            else:
+                figure_name = "/Subject_" + str(Idx_subject) + "_Finger_"+str(Finger)+"_baseline"
+                PATH_pre_trained = checkpoint_path + '/s'+ str(Idx_subject) + '_f'+str(Finger)+'_trained_model'
             if pre_trained:
-                net.load_state_dict(torch.load('f'+str(Finger)+'_trained_model'))
+                net.load_state_dict(torch.load(PATH_pre_trained))
 
             else:
-
+                net.train()
                 try:
-                    corr_train, corr_val, corr_test = train(TrainX, TrainY,TestX,TestY, net, lossfunc, optimizer, num_epoch = 70, clip = 5, Finger=Finger)
+                    corr_train, corr_val, corr_test = train(TrainX, TrainY,TestX,TestY, net, lossfunc, optimizer, num_epoch = 200, clip = 5, Finger=Finger)
                 except KeyboardInterrupt:
                     #save the model
                     print("saving...")
-                PATH_pre_trained = 'f'+str(Finger)+'_trained_model'
+                
                 torch.save(net.state_dict(), PATH_pre_trained)
                 print("model saved")
 
-            ##test initial model
+            ##test baseline model
             net.eval()
             pred,h = net(torch.from_numpy(TestX).float(), net.init_hidden(TestX.shape[0]))
+
+            
+
+            ############################################BINARIZATON&FP_QUANTIZATION#########################################################################
+            # print("Fixed Point Quantization======================================================================")
+            # net.eval()
+            # bin_pred, h = net(torch.from_numpy(TestX).float(), net.init_hidden(TestX.shape[0]), bin_=True)
+            # bin_pred = bin_pred[-1,:,:].detach().numpy().reshape((-1,))
+            # bin_corrcoef = np.corrcoef(bin_pred,TestY.reshape((-1,)))
+            
+            # print ('Correlation coefficient test : {corrcoef}'.format(corrcoef=bin_corrcoef[0,1]))   
+
+            ##############################################PRUNING###########################################################################
+            if pruning:
+                print("Pruning============================================================================")
+                PATH_pre_trained_prun = checkpoint_path + '/s'+ str(Idx_subject) + '_f'+str(Finger)+'_trained_pruned_model'
+                figure_name = "/Subject_" + str(Idx_subject) + "_Finger_"+str(Finger)+"_pruning"
+
+                if pre_trained:
+                    net.load_state_dict(torch.load(PATH_pre_trained_prun))
+                else:
+                    net.train()
+                    net.threshold_pruning()
+                    #train the prunned model:
+                    try:
+                        corr_train, corr_val, corr_test = train(TrainX, TrainY, TestX, TestY, net, lossfunc, optimizer, num_epoch=10, clip = 5, Finger = Finger)
+                    except KeyboardInterrupt:
+                        #save the model
+                        print("saving...")
+                    
+                    torch.save(net.state_dict(), PATH_pre_trained_prun)
+                    print("trained pruned model saved")
+
+                net.eval()
+                pred,h = net(torch.from_numpy(TestX).float(), net.init_hidden(TestX.shape[0]))
+                # prun_pred = prun_pred[-1,:,:].detach().numpy().reshape((-1,))
+                # prun_corrcoef = np.corrcoef(prun_pred,TestY.reshape((-1,)))
+                # print ('Correlation coefficient test : {corrcoef}'.format(corrcoef=prun_corrcoef[0,1]))   
+
+            ##############################################TRAINED QUANTIZATION##############################################################
+            elif trained_quantization:
+                print("Trained Quantization===================================================================")
+                PATH_pre_trained_quant = checkpoint_path + '/s'+ str(Idx_subject) + '_f'+str(Finger)+'_trained_quantized_model'
+                figure_name = "/Subject_" + str(Idx_subject) + "_Finger_"+str(Finger)+"_trained_quant"
+                if pre_trained:
+                    net.load_state_dict(torch.load(PATH_pre_trained_quant))
+                else:
+                    net.load_state_dict(torch.load(PATH_pre_trained))
+                    k=8
+                    #initialize the quantiezed weights using the weights from the trained netwrok:
+                    net = compute_quantized_weights(net,k)
+                    net.train()
+                    
+                    #train the quantized netwok
+                    quantized_corr_train, quantized_corr_val, quantized_corr_test = quantized_train(TrainX, TrainY,TestX,TestY, net, lossfunc, optimizer, num_epoch = 60, clip = 5)
+                    #set the model's parameters to their quantized version
+                    net = quantize_network(net)
+                    torch.save(net.state_dict(), PATH_pre_trained_quant)
+                    print("trained quantized model saved!")
+
+                    
+
+                net.eval()
+                pred,h = net(torch.from_numpy(TestX).float(), net.init_hidden(TestX.shape[0]), quant=True)
+                # quant_pred = quant_pred.detach().numpy()[-1,:,:]
+                # quant_pred = y_scaler.inverse_transform(quant_pred)
+                # quant_pred = quant_pred.reshape((-1,))
+                # quant_corrcoef = np.corrcoef(quant_pred,TestY.reshape((-1,)))
+
+                # TestYShifted = TestY
+                # #pred_tf = scaler.inverse_transform(pred)
+                # x = np.arange(TestYShifted.shape[0])
+                
+            
+                # fig_label = plt.figure(figsize=(15,10))
+                # plt.title("Subject_" + str(Idx_subject) + "_Finger_"+str(Finger))
+                # plt.plot(x, TestYShifted)
+                # plt.plot(x, quant_pred)
+                # fig_label.savefig(figure_path + "/Subject_" + str(Idx_subject) + "_Finger_"+str(Finger) + "Quantized")
+
+                # print ('Correlation coefficient test : {corrcoef}'.format(corrcoef=quant_corrcoef[0,1]))   
+            
             pred = pred.detach().numpy()[-1,:,:]
             pred = y_scaler.inverse_transform(pred)
             TestY = y_scaler.inverse_transform(TestY)
@@ -117,118 +210,7 @@ for Idx_subject in list([10]): # 3 subjects index 10-12
             plt.title("Subject_" + str(Idx_subject) + "_Finger_"+str(Finger))
             plt.plot(x, TestYShifted)
             plt.plot(x, pred)
-            fig_label.savefig(figure_path + "/Subject_" + str(Idx_subject) + "_Finger_"+str(Finger))
+            fig_label.savefig(figure_path + figure_name)
 
             
             print ('Correlation coefficient test : {corrcoef}'.format(corrcoef=corrcoef[0,1]))   
-
-
-
-
-            ##############################################FIXED POINT QUANTIZATION###########################################################################
-
-            #net = LSTM(input_dim, output_dim, seq_len,  n_hidden, n_layers, fixed_pt_quantize = True)
-            #net.train()
-
-            ##training the initial model
-            #if pre_trained:
-            #    net.load_state_dict(torch.load('f'+str(Finger)+'_trained_model'))
-
-            #else:
-
-            #    try:
-            #        corr_train, corr_val, corr_test = train(TrainX, TrainY,TestX,TestY, net, lossfunc, optimizer, num_epoch = 70, clip = 5, Finger=Finger)
-            #    except KeyboardInterrupt:
-                    #save the model
-            #        print("saving...")
-            #    PATH_pre_trained = 'f'+str(Finger)+'_trained_model'
-            #    torch.save(net.state_dict(), PATH_pre_trained)
-            #    print("model saved")
-
-            ##test initial model
-            #net.eval()
-            #pred,h = net(torch.from_numpy(TestX).float(), net.init_hidden(TestX.shape[0]))
-            #pred = pred.detach().numpy()[-1,:,:]
-            #pred = y_scaler.inverse_transform(pred)
-            #TestY = y_scaler.inverse_transform(TestY)
-            #pred = pred.reshape((-1,))
-            #corrcoef = np.corrcoef(pred,TestY.reshape((-1,)))
-
-            #TestYShifted = TestY
-            #pred_tf = scaler.inverse_transform(pred)
-            #x = np.arange(TestYShifted.shape[0])
-           
-            #fig_label = plt.figure(figsize=(15,10))
-            #plt.title("Subject_" + str(Idx_subject) + "_Finger_"+str(Finger))
-            #plt.plot(x, TestYShifted)
-            #plt.plot(x, pred)
-            #fig_label.savefig(figure_path + "/Subject_" + str(Idx_subject) + "_Finger_"+str(Finger))
-
-            
-            #print ('Correlation coefficient test : {corrcoef}'.format(corrcoef=corrcoef[0,1]))   
-
-            ##############################################PRUNING###########################################################################
-            # print("Pruning============================================================================")
-            
-
-            # if pre_trained:
-            #     net.load_state_dict(torch.load('f'+str(Finger)+'_trained_pruned_model'))
-            # else:
-            #     net.train()
-            #     net.threshold_pruning()
-            #     #train the prunned model:
-            #     try:
-            #         corr_train, corr_val, corr_test = train(TrainX, TrainY, TestX, TestY, net, lossfunc, optimizer, num_epoch=10, clip = 5, Finger = Finger)
-            #     except KeyboardInterrupt:
-            #         #save the model
-            #         print("saving...")
-            #     PATH_pre_trained = 'f'+str(Finger)+'_trained_pruned_model'
-            #     torch.save(net.state_dict(), PATH_pre_trained)
-            #     print("trained pruned model saved")
-
-            # net.eval()
-            # prun_pred,h = net(torch.from_numpy(TestX).float(), net.init_hidden(TestX.shape[0]))
-            # prun_pred = prun_pred[-1,:,:].detach().numpy().reshape((-1,))
-            # prun_corrcoef = np.corrcoef(prun_pred,TestY.reshape((-1,)))
-            # print ('Correlation coefficient test : {corrcoef}'.format(corrcoef=prun_corrcoef[0,1]))   
-
-            ##############################################TRAINED QUANTIZATION##############################################################
-            print("Trained Quantization===================================================================")
-            
-            if pre_trained:
-                net.load_state_dict(torch.load('f'+str(Finger)+'_trained_quantized_model'))
-            else:
-                k=8
-                #initialize the quantiezed weights using the weights from the trained netwrok:
-                net = compute_quantized_weights(net,k)
-                net.train()
-                
-                #train the quantized netwok
-                quantized_corr_train, quantized_corr_val, quantized_corr_test = quantized_train(TrainX, TrainY,TestX,TestY, net, lossfunc, optimizer, num_epoch = 30, clip = 5)
-                #set the model's parameters to their quantized version
-                net = quantize_network(net)
-                torch.save(net.state_dict(), 'f'+str(Finger)+'_trained_quantized_model')
-                print("trained quantized model saved!")
-
-                
-
-            net.eval()
-            quant_pred,h = net(torch.from_numpy(TestX).float(), net.init_hidden(TestX.shape[0]), quant=True)
-            quant_pred = quant_pred.detach().numpy()[-1,:,:]
-            quant_pred = y_scaler.inverse_transform(quant_pred)
-            quant_pred = quant_pred.reshape((-1,))
-            quant_corrcoef = np.corrcoef(quant_pred,TestY.reshape((-1,)))
-
-            TestYShifted = TestY
-            #pred_tf = scaler.inverse_transform(pred)
-            x = np.arange(TestYShifted.shape[0])
-            
-           
-            fig_label = plt.figure(figsize=(15,10))
-            plt.title("Subject_" + str(Idx_subject) + "_Finger_"+str(Finger))
-            plt.plot(x, TestYShifted)
-            plt.plot(x, quant_pred)
-            fig_label.savefig(figure_path + "/Subject_" + str(Idx_subject) + "_Finger_"+str(Finger) + "Quantized")
-
-            print ('Correlation coefficient test : {corrcoef}'.format(corrcoef=quant_corrcoef[0,1]))   
-            
